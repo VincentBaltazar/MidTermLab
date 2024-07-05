8899276225f26885361b12f5fd246eb5e2c56526
Vincent Rafael Baltazar <21-74089@g.batstate-u.edu.ph>
2024-07-05 11:29:19 +0800

    merge the file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,5 @@
 # file1.py
+<<<<<<< HEAD
 def fibonacci_sequence(n):
     sequence = [0, 1]
     while len(sequence) < n:
@@ -6,3 +7,12 @@ def fibonacci_sequence(n):
     return sequence
     result = fibonacci_sequence(10)
 print(f"Fibonacci sequence up to 10 numbers: {result}")
+=======
+def factorial(n):
+ if n == 0:
+ return 1
+ else:
+ return n * factorial(n-1)
+result = factorial(5)
+print(f"Factorial of 5 is: {result}")
+>>>>>>> parent of 68c41af (fixed the bugs)
